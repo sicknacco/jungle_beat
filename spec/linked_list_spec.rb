8f39c8da169945c6138aa5488c8ff2ca91668d63
Nick Sacco <nicksacco10@gmail.com>
2023-02-05 12:42:07 -0800

    Test: Add test for prepend method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -40,8 +40,17 @@ RSpec.describe LinkedList do
     expect(list.to_string).to eq("doop")
   end
 
-#prepend test will go here
 
+  it 'can prepend' do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+
+    expect(list.head.data).to eq("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+  end
 
 
 
