e321364f3f0daa60135f7025fcdc7a9600784a32
Nick Sacco <nicksacco10@gmail.com>
2023-02-05 10:10:27 -0800

    Fix: append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,23 +7,31 @@ class LinkedList
 
   def append(data)
     @count += 1 #adding 1 for every append
-    new_node = Node.new(data)
-    if @head.nil?
-      @head = new_node #set as on ln 9, now head
+    if @head.nil? #if the head is nill, do the following
+       @head = Node.new(data) #@head is a new node
+    else
+      current_node = @head #assigns current_node as head
+      until current_node.next_node.nil? #until c_n.n_n is false
+        current_node = current_node.next_node #c_n is head. 
+      end
+      current_node.next_node = Node.new(data)
     end
     return data #shows list.append as "doop"
   end
 
+
   def to_string
-    string = "" #empty placeholder
     current_node = @head #current_node is the head
-    until current_node.nil? #Until this is true, do the following 
+    string = " " #empty placeholder for below
+    until current_node.nil? #Until this is true, do the following :
       string << "#{current_node.data} " #shoves current node's data into string
       current_node = current_node.next_node #makes current_node to head. Then goes to next node
     end
-    string.strip #Removes last space so 'until' is nill? (CONFIRM THIS!!)
+    string.strip #Removes empty spaces from string
   end
 
 
-
+#### Currently on list.head.next_node
+### Output Desired => <Node data="deep" next_node=nil #5678904567890>
+### Currently get nil
 end
