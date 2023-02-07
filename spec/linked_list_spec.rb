da282f15afc323d146ced905da4b054d3b538899
Nick Sacco <nicksacco10@gmail.com>
2023-02-07 13:18:03 -0800

    Test: Complete includes? test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -77,6 +77,17 @@ RSpec.describe LinkedList do
     expect(list.find(1, 3)).to eq("woo shi shu")
  end
 
+  it 'can test if list has certain data' do
+      list = LinkedList.new
+      list.append("deep")
+      list.append("woo")
+      list.append("shi")
+      list.append("shu")
+      list.append("blop")
+
+      expect(list.includes?("deep")).to eq(true)
+      expect(list.includes?("dep")).to eq(false)
+  end
 
   
 
