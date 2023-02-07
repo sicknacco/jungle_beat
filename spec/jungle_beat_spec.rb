ed41d6c427f0deb2951ccd29180238768fda3961
Nick Sacco <nicksacco10@gmail.com>
2023-02-07 15:57:45 -0800

    Test: Complete append multiple test

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -10,7 +10,12 @@ RSpec.describe JungleBeat do
     expect(jb).to be_instance_of(JungleBeat)
     expect(jb.list).to be_instance_of(LinkedList)
     expect(jb.list.head).to eq(nil)
+  end
 
+  it 'can append multiple items' do
+    jb = JungleBeat.new
+
+    expect(jb.append("deep doo ditt")).to eq("deep doo ditt")
   end
 
 
