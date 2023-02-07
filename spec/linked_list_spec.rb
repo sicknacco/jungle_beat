13943aae04a2d6a36b6b61caf852f0343db15e17
Nick Sacco <nicksacco10@gmail.com>
2023-02-07 07:45:23 -0800

    Test: Complete .find test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -63,7 +63,19 @@ RSpec.describe LinkedList do
     expect(list.count).to eq(4)
  end
 
+ it 'can find and return data' do
+    list = LinkedList.new
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
 
+    expect(list.head.data).to eq("deep")
+    expect(list.to_string).to eq("deep woo shi shu blop")
+    expect(list.find(2, 1)).to eq("shi")
+    expect(list.find(1, 3)).to eq("woo shi shu")
+ end
 
 
   
